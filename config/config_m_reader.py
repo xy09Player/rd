34ccfdf5680382b6dd423ec7fa9f53d66bcef23f
xy09Player <2106401573@qq.com>
2018-10-20 16:57:54 +0800

    version2.3(参数设置)

--- config/config_m_reader.py
+++ config/config_m_reader.py
@@ -6,16 +6,18 @@ from config import config_base
 
 class Config(config_base.ConfigBase):
     model_name = 'm_reader'
-    model_save = model_name + '_2'
+    model_save = model_name + '_1'
     is_bn = True
-    epoch = 12
+    epoch = 15
     mode = 'LSTM'
     batch_size = 32
     hidden_size = 100
     encoder_layer_num = 1
-    dropout_p = 0.1
+    dropout_p = 0.2
     val_every = 100
     val_mean = False
+
+    val_split_value = 1.1
 
     # 联合训练
     is_for_rouge = False
@@ -25,7 +27,9 @@ class Config(config_base.ConfigBase):
         val_mean = True
 
     # 测试
-    model_test = 'm_reader_3_mrt'
-    is_true_test = True
+    model_test = 'm_reader_1'
+    gen_result = True
+    is_true_test = False
+    test_batch_size = 64
 
 config = Config()
